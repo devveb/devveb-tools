1583006f4b1b929c8d3d2f7600b5711289ee1f24
taehyeong.kim <taehyeong.kim@yap.net>
2025-07-03 23:50:30 +0900

    fix

--- backend/src/main/java/com/sbsoft/dvtbck/backend/config/WebConfig.java
+++ backend/src/main/java/com/sbsoft/dvtbck/backend/config/WebConfig.java
@@ -11,5 +11,10 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("*");
         registry.addMapping("/**").allowedOrigins("http://localhost:5173").allowedMethods("*");
+        registry.addMapping("/**") // 모든 경로에 대해
+                .allowedOrigins("https://devveb-tools-1.onrender.com") // 프론트 도메인
+                .allowedMethods("*") // GET, POST 등 모두 허용
+                .allowedHeaders("*")
+                .allowCredentials(false);
     }
 }
